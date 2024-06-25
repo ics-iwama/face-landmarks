6c86daca9aba32116bb7cd273332489655595575
ics-iwama <iwama@ics-web.jp>
2024-06-25 12:18:36 +0900

    コードの修正

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.165.0/build/three.module.js';
 
 // グローバルな変数定義
-let decoImageList = ['hige', 'ribbon', 'rabbit', 'cat02', 'cat03', 'bear01']; // loadする画像のリスト
-let decoLoadedImage = {}; // loadした画像を格納する
+let decoLoadedImage = {}; // スタンプ画像を格納するオブジェクト
 let decoImageRatios = {}; // 画像の縦横比を格納するオブジェクト
-let buttonElements = document.querySelectorAll('.button');
-let positionButtonElements = document.querySelectorAll('.position-controllerButton');
 let currentDeco = 'rabbit'; // デフォルトのスタンプ
+const decoImageList = ['hige', 'ribbon', 'rabbit', 'cat02', 'cat03', 'bear01']; // スタンプ画像のリスト
+const buttonElements = document.querySelectorAll('.button');
+const positionButtonElements = document.querySelectorAll('.position-controllerButton');
 
-const videoWidth = 640;
-const videoHeight = 480;
 const canvasEl = document.querySelector('#canvas');
 const videoEl = document.querySelector('#video');
+const videoWidth = 640;
+const videoHeight = 480;
 let decoMesh;
 let detector;
 let results; // 検出した顔のリスト
@@ -27,19 +27,19 @@ const decoSettings = {
   hige: { scale: 30, basePoint: 164, xFix: 5, yFix: -20 },
   rabbit: { scale: 280, basePoint: 1, xFix: 10, yFix: -30 },
   ribbon: { scale: 70, basePoint: 0, xFix: 5, yFix: -5 },
-  cat02: { scale: 180, basePoint: 1, xFix: 5, yFix: -20 },
+  cat02: { scale: 210, basePoint: 1, xFix: 5, yFix: -20 },
   cat03: { scale: 190, basePoint: 1, xFix: 0, yFix: 0 },
-  bear01: { scale: 180, basePoint: 1, xFix: 0, yFix: 0 }
+  bear01: { scale: 180, basePoint: 1, xFix: 0, yFix: 0 },
 };
 
 // Three.jsの初期設定を行う関数
 function setupTHREE() {
   renderer = new THREE.WebGLRenderer({
     canvas: canvasEl,
-    alpha: true // canvasの背景を透明にするために設定
+    alpha: true, // canvasの背景を透明にするために設定
   });
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(videoWidth, videoHeight);
+  renderer.setSize( videoWidth, videoHeight);
   renderer.setClearColor(0x000000, 0);
 
   // シーンを作成
@@ -111,7 +111,7 @@ function createDecoPlane() {
   const material = new THREE.MeshBasicMaterial({
     map: texture,
     side: THREE.DoubleSide,
-    transparent: true
+    transparent: true,
   });
 
   decoMesh = new THREE.Mesh(geometry, material);
@@ -135,52 +135,60 @@ function updateDecoPlane() {
 
 // スタンプのためのplaneの位置と回転、スケールを更新
 function updateDecoMesh() {
-  if (results && results.length > 0) {
-    const quaternion = calcNormalVector(); // 顔の向きのベクトル（法線ベクトル）から回転を計算
-    decoMesh.quaternion.copy(quaternion); // 向きを合わせる
-
-    const settings = decoSettings[currentDeco];
-    const fixData = fixLandmarkValue(results[0].keypoints); // Three.jsで使える座標にする
-    const basePoint = fixData[settings.basePoint];
-    const faceCenter = new THREE.Vector3(
-      basePoint.x + positionX + settings.xFix,
-      basePoint.y + positionY + settings.yFix,
-      basePoint.z - 150,
-    );
-
-    const noseTip = fixData[1];  // インデックス1が鼻の中央に対応
-    const rightEar = fixData[127]; // インデックス127が右耳に対応
-    const leftEar = fixData[356];  // インデックス356が左耳に対応
-
-    // 顔の傾きを計算する
-    const dx = rightEar.x - leftEar.x;
-    const dy = rightEar.y - leftEar.y;
-    const angle = Math.atan2(dy, dx);
-
-    // 鼻の中央から左右の耳までの距離を計算する
-    const distanceToRightEar = Math.sqrt(Math.pow(noseTip.x - rightEar.x, 2) + Math.pow(noseTip.y - rightEar.y, 2));
-    const distanceToLeftEar = Math.sqrt(Math.pow(noseTip.x - leftEar.x, 2) + Math.pow(noseTip.y - leftEar.y, 2));
-    const earDistanceSum = distanceToRightEar + distanceToLeftEar;
-    const baseEarDistanceSum = 200; // 基準となる鼻の中央から左右の耳までの距離の絶対値の合計
-
-    // 鼻の中央から左右の耳までの距離の絶対値の合計に基づいてスケールを計算する（横向いたときに画像が小さくならないように）
-    const scale = earDistanceSum / baseEarDistanceSum;
-
-    decoMesh.scale.set(settings.scale * scale, -settings.scale * scale, 0); // スケールを適用
-
-    // 画像の高さの半分を計算して、Y軸の位置を調整
-    const imageHeight = settings.scale * scale; // PlaneGeometryの高さ * スケール
-    faceCenter.y += imageHeight / 2; // 高さの半分をY軸に加算
-
-    // 画像の回転中心を画像の中心に設定するため、X軸およびY軸方向のずれを調整
-    const offsetX = imageHeight / 2 * Math.sin(angle); // 回転に伴うX方向のずれを計算
-    faceCenter.x += offsetX; // X方向のずれを調整
-    faceCenter.y -= offsetX * Math.sin(angle); // Y方向のずれを調整
-
-    decoMesh.position.copy(faceCenter);
-
-    decoMesh.rotation.z = angle; // 画像を回転
+  if (!results || results.length === 0) {
+    return;
   }
+  const quaternion = calcNormalVector(); // 顔の向きのベクトル（法線ベクトル）から回転を計算
+  decoMesh.quaternion.copy(quaternion); // 向きを合わせる
+
+  const settings = decoSettings[currentDeco];
+  const fixData = fixLandmarkValue(results[0].keypoints); // Three.jsで使える座標にする
+  const basePoint = fixData[settings.basePoint];
+
+  // スマホのインカメラを使用しているかを判定
+  const userAgent = navigator.userAgent.toLowerCase();
+  const isMobile = /iphone|ipad|ipod|android/.test(userAgent);
+  const isFrontCamera = videoEl.srcObject.getTracks()[0].getSettings().facingMode === 'user';
+  const zOffset = isMobile && isFrontCamera ? 200 : 120;
+
+  const faceCenter = new THREE.Vector3(
+    basePoint.x + positionX + settings.xFix,
+    basePoint.y + positionY + settings.yFix,
+    basePoint.z - zOffset,
+  );
+
+  const noseTip = fixData[1];  // インデックス1が鼻の中央に対応
+  const rightEar = fixData[127]; // インデックス127が右耳に対応
+  const leftEar = fixData[356];  // インデックス356が左耳に対応
+
+  // 顔の傾きを計算する
+  const dx = rightEar.x - leftEar.x;
+  const dy = rightEar.y - leftEar.y;
+  const angle = Math.atan2(dy, dx);
+
+  // 鼻の中央から左右の耳までの距離を計算する
+  const distanceToRightEar = Math.hypot((noseTip.x - rightEar.x), (noseTip.y - rightEar.y));
+  const distanceToLeftEar = Math.hypot((noseTip.x - leftEar.x), (noseTip.y - leftEar.y));
+  const earDistanceSum = distanceToRightEar + distanceToLeftEar;
+  const baseEarDistanceSum = 200; // 基準となる鼻の中央から左右の耳までの距離の絶対値の合計
+
+  // 鼻の中央から左右の耳までの距離の絶対値の合計に基づいてスケールを計算する（横向いたときに画像が小さくならないように）
+  const scale = earDistanceSum / baseEarDistanceSum;
+
+  decoMesh.scale.set(settings.scale * scale, -settings.scale * scale, 0); // スケールを適用
+
+  // 画像の高さの半分を計算して、Y軸の位置を調整
+  const imageHeight = settings.scale * scale; // PlaneGeometryの高さ * スケール
+  faceCenter.y += imageHeight / 2; // 高さの半分をY軸に加算
+
+  // 画像の回転中心を画像の中心に設定するため、X軸およびY軸方向のずれを調整
+  const offsetX = imageHeight / 2 * Math.sin(angle); // 回転に伴うX方向のずれを計算
+  faceCenter.x += offsetX; // X方向のずれを調整
+  faceCenter.y -= offsetX * Math.sin(angle); // Y方向のずれを調整
+
+  decoMesh.position.copy(faceCenter);
+
+  decoMesh.rotation.z = angle; // 画像を回転
 }
 
 // シーンをレンダリングする関数
@@ -194,32 +202,40 @@ function render() {
 }
 
 function calcNormalVector() {
-  if (results && results.length > 0) {
-    const fixData = fixLandmarkValue(results[0].keypoints);
-    const noseTip = fixData[1];
-    const leftNose = fixData[279];
-    const rightNose = fixData[49];
-
-    const midpoint = {
-      x: (leftNose.x + rightNose.x) / 2,
-      y: (leftNose.y + rightNose.y) / 2,
-      z: (leftNose.z + rightNose.z) / 2
-    };
-    const perpendicularUp = {
-      x: midpoint.x,
-      y: midpoint.y - 10,
-      z: midpoint.z,
-    };
-
-    faceNormalVector = new THREE.Vector3(noseTip.x, noseTip.y, noseTip.z)
-      .sub(new THREE.Vector3(perpendicularUp.x, perpendicularUp.y, perpendicularUp.z))
-      .normalize();
-
-    const quaternion = new THREE.Quaternion();
-    quaternion.setFromUnitVectors(new THREE.Vector3(0, 0, 1.0), faceNormalVector);
-
-    return quaternion;
+  if (!results || results.length === 0) {
+    return;
   }
+
+  // 顔のキーポイントデータを取得し、Three.jsで使用できる座標に変換
+  const fixData = fixLandmarkValue(results[0].keypoints);
+  const noseTip = fixData[1]; // 鼻の先端のキーポイント
+  const leftNose = fixData[279]; // 鼻の左端のキーポイント
+  const rightNose = fixData[49]; // 鼻の右端のキーポイント
+
+  // 鼻の左端と右端の中間点を計算
+  const midpoint = {
+    x: (leftNose.x + rightNose.x) / 2,
+    y: (leftNose.y + rightNose.y) / 2,
+    z: (leftNose.z + rightNose.z) / 2
+  };
+
+  // 中間点から垂直方向に少し上の点を定義
+  const perpendicularUp = {
+    x: midpoint.x,
+    y: midpoint.y - 10,
+    z: midpoint.z,
+  };
+
+  // 垂直方向に上の点（perpendicularUp）から鼻の先端（noseTip）までのベクトルを計算し、正規化して法線ベクトルを取得
+  faceNormalVector = new THREE.Vector3(noseTip.x, noseTip.y, noseTip.z)
+    .sub(new THREE.Vector3(perpendicularUp.x, perpendicularUp.y, perpendicularUp.z))
+    .normalize();
+
+  // 法線ベクトルを基にクォータニオンを作成（Z軸から法線ベクトルへの回転を表す）
+  const quaternion = new THREE.Quaternion();
+  quaternion.setFromUnitVectors(new THREE.Vector3(0, 0, 1), faceNormalVector);
+
+  return quaternion;
 }
 
 // Webカメラを有効にする関数
@@ -228,7 +244,7 @@ async function enableWebcam() {
     video: {
       width: videoWidth,
       height: videoHeight,
-      facingMode: 'user' // スマホのフロントカメラを使用
+      facingMode: 'user', // スマホのフロントカメラを使用
     },
     audio: false,
   };
